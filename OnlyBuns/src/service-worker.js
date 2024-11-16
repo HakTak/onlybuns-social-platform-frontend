e1d8b1df4652cf2488cc7abfb27f7ce070450fa5
pili01 <pilipovicdusko002@gmail.com>
2024-11-16 17:34:43 +0100

    removed console logs

--- OnlyBuns/src/service-worker.js
+++ OnlyBuns/src/service-worker.js
@@ -2,16 +2,16 @@ const CACHE_NAME = 'image-cache-v1';
 const IMAGE_URL_PATTERN = /\/api\/posts\/images\/.*\.(?:png|jpg|jpeg|gif|svg)$/;
 
 self.addEventListener('install', event => {
-  console.log('Service Worker installing.');
+  //console.log('Service Worker installing.');
   // Skip waiting to activate the new service worker immediately
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch event for:', event.request.url);
+  //console.log('Fetch event for:', event.request.url);
   // Check if the request URL matches the image pattern
   if (IMAGE_URL_PATTERN.test(event.request.url)) {
-    console.log('Fetching image:', event.request.url);
+    //console.log('Fetching image:', event.request.url);
     event.respondWith(
       caches.match(event.request)
         .then(response => {
@@ -19,19 +19,19 @@ self.addEventListener('fetch', event => {
             console.log('Found in cache:', event.request.url);
             return response;
           }
-          console.log('Not found in cache, fetching from network:', event.request.url);
+          //console.log('Not found in cache, fetching from network:', event.request.url);
           return fetch(event.request, { mode: 'cors' }).then(
             response => {
-              console.log('Network response:', response);
+              //console.log('Network response:', response);
               if (!response || response.status !== 200) {
                 console.log('Network request failed or not basic:', event.request.url);
                 return response;
               }
-              console.log('Network request successful:', event.request.url);
+              //console.log('Network request successful:', event.request.url);
               const responseToCache = response.clone();
               caches.open(CACHE_NAME)
                 .then(cache => {
-                  console.log('Opened cache:', CACHE_NAME);
+                  //console.log('Opened cache:', CACHE_NAME);
                   cache.put(event.request, responseToCache)
                     .then(() => {
                       console.log('Image cached:', event.request.url);
@@ -51,12 +51,12 @@ self.addEventListener('fetch', event => {
         })
     );
   } else {
-    console.log('Request does not match image pattern:', event.request.url);
+    //console.log('Request does not match image pattern:', event.request.url);
   }
 });
 
 self.addEventListener('activate', event => {
-  console.log('Service Worker activating.');
+  //console.log('Service Worker activating.');
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
