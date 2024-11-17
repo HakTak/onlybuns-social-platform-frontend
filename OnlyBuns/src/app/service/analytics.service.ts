285ef664d6d4494897090b98662a32979f334810
pili01 <pilipovicdusko002@gmail.com>
2024-11-17 19:08:47 +0100

    service updated

--- OnlyBuns/src/app/service/analytics.service.ts
+++ OnlyBuns/src/app/service/analytics.service.ts
@@ -9,9 +9,9 @@ import { ConfigService } from './config.service';
 })
 export class AnalyticsService {
 
-  private baseUrl =this.config.analytics_url;
+  private baseUrl = this.config.analytics_url;
 
-  constructor(private http: HttpClient, private router: Router,private config: ConfigService) { }
+  constructor(private http: HttpClient, private router: Router, private config: ConfigService) { }
 
   getPostsAndCommentsAnalytics(year: number, month: string): Observable<any> {
     const token = localStorage.getItem('jwt'); // Preuzimanje tokena iz localStorage
@@ -20,6 +20,23 @@ export class AnalyticsService {
     });
     const url = month === 'all' ? `${this.baseUrl}/posts-comments?year=${year}` : `${this.baseUrl}/posts-comments?year=${year}&month=${month}`;
     return this.http.get(url, { headers }).pipe(
+      catchError(error => {
+        if (error.status === 403) {
+          // Preusmeravanje na login ako je zabranjen pristup
+          this.router.navigate(['/login']);
+          alert("You must be logged as Admin");
+        }
+        return throwError(() => error);  // Prosleđivanje greške dalje
+      })
+    );
+  }
+
+  getPostsAndCommentsByYear(): Observable<any> {
+    const token = localStorage.getItem('jwt'); // Preuzimanje tokena iz localStorage
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}` // Dodavanje tokena u Authorization header
+    });
+    return this.http.get(`${this.baseUrl}/posts-comments-by-year`, { headers }).pipe(
       catchError(error => {
         if (error.status === 403) {
           // Preusmeravanje na login ako je zabranjen pristup
